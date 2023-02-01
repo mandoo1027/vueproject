95d89a6db2e1cff92315c4a0fa918c95e07f8444
A02053@hanatour.com <40375307+mandoo1027@users.noreply.github.com>
2023-02-01 22:49:26 +0900

    커밋

--- template/pages/dashboard.vue
+++ template/pages/dashboard.vue
@@ -61,7 +61,7 @@
                   ['series[0].smooth', true],
                   ['series[1].smooth', true],
                   ['series[1].type', 'bar'],
-                  ['series[1].areaStyle', {}],
+                  ['series[1].areaStyle', {}]
                 ]"
                 height="400px"
                 width="85%"
@@ -77,12 +77,22 @@
                 :path-option="[
                   ['dataset.source', locationData],
                   ['legend.bottom', '0'],
-                  ['color', [color.lightBlue.base, color.indigo.base, color.pink.base, color.green.base, color.cyan.base, color.teal.base]],
+                  [
+                    'color',
+                    [
+                      color.lightBlue.base,
+                      color.indigo.base,
+                      color.pink.base,
+                      color.green.base,
+                      color.cyan.base,
+                      color.teal.base
+                    ]
+                  ],
                   ['xAxis.show', false],
                   ['yAxis.show', false],
                   ['series[0].type', 'pie'],
                   ['series[0].avoidLabelOverlap', true],
-                  ['series[0].radius', ['50%', '70%']],
+                  ['series[0].radius', ['50%', '70%']]
                 ]"
                 height="400px"
                 width="100%"
@@ -93,8 +103,7 @@
         </v-flex>
         <!-- social/weather card start -->
         <v-flex lg4 sm12 xs12>
-          <profile-card>
-          </profile-card>
+          <profile-card> </profile-card>
         </v-flex>
         <v-flex lg4 sm12 xs12>
           <box-chart
@@ -107,15 +116,16 @@
             type="line"
           >
           </box-chart>
-          <box-chart class="mt-4"
-                     card-color="pink"
-                     title="Page views"
-                     sub-title="10%"
-                     icon="trending_up"
-                     :data="siteTrafficData"
-                     :chart-color="[color.pink.darken1, 'rgba(255,255,255,0.3)']"
-                     gradient
-                     type="area"
+          <box-chart
+            class="mt-4"
+            card-color="pink"
+            title="Page views"
+            sub-title="10%"
+            icon="trending_up"
+            :data="siteTrafficData"
+            :chart-color="[color.pink.darken1, 'rgba(255,255,255,0.3)']"
+            gradient
+            type="area"
           >
           </box-chart>
         </v-flex>
@@ -129,33 +139,42 @@
             :value="15"
           >
           </linear-statistic>
-          <linear-statistic class="my-4"
-                            title="Orders"
-                            sub-title="Increase"
-                            icon="trending_up"
-                            color="pink"
-                            :value="30"
+          <linear-statistic
+            class="my-4"
+            title="Orders"
+            sub-title="Increase"
+            icon="trending_up"
+            color="pink"
+            :value="30"
           >
           </linear-statistic>
-          <linear-statistic class="my-4"
-                            title="Revenue"
-                            sub-title="Revenue increase"
-                            icon="trending_up"
-                            color="primary"
-                            :value="50"
+          <linear-statistic
+            class="my-4"
+            title="Revenue"
+            sub-title="Revenue increase"
+            icon="trending_up"
+            color="primary"
+            :value="50"
           >
           </linear-statistic>
-          <linear-statistic class="mt-4"
-                            title="Cost"
-                            sub-title="Cost reduce"
-                            icon="trending_down"
-                            color="orange"
-                            :value="25"
+          <linear-statistic
+            class="mt-4"
+            title="Cost"
+            sub-title="Cost reduce"
+            icon="trending_down"
+            color="orange"
+            :value="25"
           >
           </linear-statistic>
         </v-flex>
         <!-- Circle statistic -->
-        <v-flex lg4 sm12 xs12 v-for="(item,index) in trending" :key="'c-trending'+index">
+        <v-flex
+          lg4
+          sm12
+          xs12
+          v-for="(item, index) in trending"
+          :key="'c-trending' + index"
+        >
           <circle-statistic
             :title="item.subheading"
             :sub-title="item.headline"
@@ -173,13 +192,21 @@
         <v-flex lg6 sm12 xs12>
           <v-widget title="Activities" contentBg="white">
             <div slot="widget-content">
-              <ol class="timeline timeline-activity timeline-point-sm timeline-content-right">
-                <li class="timeline-block" v-for="(item, index) in activity" :key="index">
+              <ol
+                class="timeline timeline-activity timeline-point-sm timeline-content-right"
+              >
+                <li
+                  class="timeline-block"
+                  v-for="(item, index) in activity"
+                  :key="index"
+                >
                   <div class="timeline-point">
                     <v-circle dot large :color="item.color"></v-circle>
                   </div>
                   <div class="timeline-content">
-                    <time datetime="2018" class="subheading">{{item.timeString}}</time>
+                    <time datetime="2018" class="subheading">{{
+                      item.timeString
+                    }}</time>
                     <div class="py-2 text--secondary" v-html="item.text"></div>
                   </div>
                 </li>
@@ -199,147 +226,149 @@
 </template>
 
 <script>
-  import API from '@/api';
-  import EChart from '@/components/chart/echart';
-  import MiniStatistic from '@/components/widgets/statistic/MiniStatistic';
-  import PostListCard from '@/components/widgets/card/PostListCard';
-  import ProfileCard from '@/components/widgets/card/ProfileCard';
-  import PostSingleCard from '@/components/widgets/card/PostSingleCard';
-  import WeatherCard from '@/components/widgets/card/WeatherCard';
-  import PlainTable from '@/components/widgets/list/PlainTable';
-  import PlainTableOrder from '@/components/widgets/list/PlainTableOrder';
-  import VWidget from '@/components/VWidget';
-  import Material from 'vuetify/es5/util/colors';
-  import VCircle from '@/components/circle/VCircle';
-  import BoxChart from '@/components/widgets/chart/BoxChart';
-  import ChatWindow from '@/components/chat/ChatWindow';
-  import CircleStatistic from '@/components/widgets/statistic/CircleStatistic';
-  import LinearStatistic from '@/components/widgets/statistic/LinearStatistic';
+import API from "@/api";
+import EChart from "@/components/chart/echart";
+import MiniStatistic from "@/components/widgets/statistic/MiniStatistic";
+import PostListCard from "@/components/widgets/card/PostListCard";
+import ProfileCard from "@/components/widgets/card/ProfileCard";
+import PostSingleCard from "@/components/widgets/card/PostSingleCard";
+import WeatherCard from "@/components/widgets/card/WeatherCard";
+import PlainTable from "@/components/widgets/list/PlainTable";
+import PlainTableOrder from "@/components/widgets/list/PlainTableOrder";
+import VWidget from "@/components/VWidget";
+import Material from "vuetify/es5/util/colors";
+import VCircle from "@/components/circle/VCircle";
+import BoxChart from "@/components/widgets/chart/BoxChart";
+import ChatWindow from "@/components/chat/ChatWindow";
+import CircleStatistic from "@/components/widgets/statistic/CircleStatistic";
+import LinearStatistic from "@/components/widgets/statistic/LinearStatistic";
 
-  export default {
-    layout: 'dashboard',
-    components: {
-      VWidget,
-      MiniStatistic,
-      ChatWindow,
-      VCircle,
-      WeatherCard,
-      PostSingleCard,
-      PostListCard,
-      ProfileCard,
-      EChart,
-      BoxChart,
-      CircleStatistic,
-      LinearStatistic,
-      PlainTable,
-      PlainTableOrder
-    },
-    data: () => ({
-      color: Material,
-      selectedTab: 'tab-1',
-      linearTrending: [
-        {
-          subheading: 'Sales',
-          headline: '2,55',
-          caption: 'increase',
-          percent: 15,
-          icon: {
-            label: 'trending_up',
-            color: 'success'
-          },
-          linear: {
-            value: 15,
-            color: 'success'
-          }
+export default {
+  layout: "dashboard",
+  components: {
+    VWidget,
+    MiniStatistic,
+    ChatWindow,
+    VCircle,
+    WeatherCard,
+    PostSingleCard,
+    PostListCard,
+    ProfileCard,
+    EChart,
+    BoxChart,
+    CircleStatistic,
+    LinearStatistic,
+    PlainTable,
+    PlainTableOrder
+  },
+  created() {
+    console.log(this.$route.params.username);
+  },
+  data: () => ({
+    color: Material,
+    selectedTab: "tab-1",
+    linearTrending: [
+      {
+        subheading: "Sales",
+        headline: "2,55",
+        caption: "increase",
+        percent: 15,
+        icon: {
+          label: "trending_up",
+          color: "success"
         },
-        {
-          subheading: 'Revenue',
-          headline: '6,553',
-          caption: 'increase',
-          percent: 10,
-          icon: {
-            label: 'trending_down',
-            color: 'error'
-          },
-          linear: {
-            value: 15,
-            color: 'error'
-          }
-        },
-        {
-          subheading: 'Orders',
-          headline: '5,00',
-          caption: 'increase',
-          percent: 50,
-          icon: {
-            label: 'arrow_upward',
-            color: 'info'
-          },
-          linear: {
-            value: 50,
-            color: 'info'
-          }
+        linear: {
+          value: 15,
+          color: "success"
         }
-      ],
-      trending: [
-        {
-          subheading: 'Email',
-          headline: '15+',
-          caption: 'email opens',
-          percent: 15,
-          icon: {
-            label: 'email',
-            color: 'info'
-          },
-          linear: {
-            value: 15,
-            color: 'info'
-          }
-        },
-        {
-          subheading: 'Tasks',
-          headline: '90%',
-          caption: 'tasks completed.',
-          percent: 90,
-          icon: {
-            label: 'list',
-            color: 'primary'
-          },
-          linear: {
-            value: 90,
-            color: 'success'
-          }
-        },
-        {
-          subheading: 'Issues',
-          headline: '100%',
-          caption: 'issues fixed.',
-          percent: 100,
-          icon: {
-            label: 'bug_report',
-            color: 'primary'
-          },
-          linear: {
-            value: 100,
-            color: 'error'
-          }
-        },
-      ]
-    }),
-    computed: {
-      activity () {
-        return API.getActivity();
       },
-      posts () {
-        return API.getPost(3);
+      {
+        subheading: "Revenue",
+        headline: "6,553",
+        caption: "increase",
+        percent: 10,
+        icon: {
+          label: "trending_down",
+          color: "error"
+        },
+        linear: {
+          value: 15,
+          color: "error"
+        }
       },
-      siteTrafficData () {
-        return API.getMonthVisit;
-      },
-      locationData () {
-        return API.getLocation;
+      {
+        subheading: "Orders",
+        headline: "5,00",
+        caption: "increase",
+        percent: 50,
+        icon: {
+          label: "arrow_upward",
+          color: "info"
+        },
+        linear: {
+          value: 50,
+          color: "info"
+        }
       }
+    ],
+    trending: [
+      {
+        subheading: "Email",
+        headline: "15+",
+        caption: "email opens",
+        percent: 15,
+        icon: {
+          label: "email",
+          color: "info"
+        },
+        linear: {
+          value: 15,
+          color: "info"
+        }
+      },
+      {
+        subheading: "Tasks",
+        headline: "90%",
+        caption: "tasks completed.",
+        percent: 90,
+        icon: {
+          label: "list",
+          color: "primary"
+        },
+        linear: {
+          value: 90,
+          color: "success"
+        }
+      },
+      {
+        subheading: "Issues",
+        headline: "100%",
+        caption: "issues fixed.",
+        percent: 100,
+        icon: {
+          label: "bug_report",
+          color: "primary"
+        },
+        linear: {
+          value: 100,
+          color: "error"
+        }
+      }
+    ]
+  }),
+  computed: {
+    activity() {
+      return API.getActivity();
     },
-
-  };
+    posts() {
+      return API.getPost(3);
+    },
+    siteTrafficData() {
+      return API.getMonthVisit;
+    },
+    locationData() {
+      return API.getLocation;
+    }
+  }
+};
 </script>
